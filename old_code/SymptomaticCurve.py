04fa924c708f40b948cb4f71f9d709c562288a22
kevryl <kevin_rylander@hotmail.com>
2021-04-21 16:55:46 +0200

    Update SymptomaticCurve.py

--- old_code/SymptomaticCurve.py
+++ old_code/SymptomaticCurve.py
@@ -39,12 +39,8 @@ plt.ylabel("Symptomatic")
 import numpy as np
 import matplotlib.pyplot as plt
 
-virus = 1000
-tcell = 0
-mcell = 100
-
 parameterTimeChanger = 10
-a = 1/parameterTimeChanger #np.random.normal(2,0.2)
+a = 0.7/parameterTimeChanger #np.random.normal(2,0.2)
 b = 0.5/parameterTimeChanger
 c = 0.1/parameterTimeChanger # np.random.normal(1,0.1)
 d = 0.001/parameterTimeChanger
@@ -52,54 +48,53 @@ e = 3/parameterTimeChanger
 f = 0.5/parameterTimeChanger
 g = 0.1/parameterTimeChanger
 
-virusPlot = [virus]
-tcellPlot = [tcell]
-mcellPlot = [mcell]
-
-
+maxPlasma = 0
 time = 100
-for ix in range(time):
-    dvdt = a*virus - b*tcell
-    dTdt = virus*(c + d*mcell) - e*tcell
-    dMdt = f*tcell - g*mcell
-    virus = virus + dvdt
-    tcell = tcell + dTdt
-    mcell = mcell + dMdt
-    if virus < 10: virus = 0
-    if tcell < 10: tcell = 0
-    if mcell < 10: mcell = 0 
-    # elif ix == 750:
-    #     virus = virus + 500
-    virusPlot.append(virus)
-    tcellPlot.append(tcell)
-    mcellPlot.append(mcell)
 
-    
+for count, c in enumerate([0.2/parameterTimeChanger,1/parameterTimeChanger]):       
+    virus = 1000
+    plasma = 0
+    mcell = 100
+    virusPlot = [virus]
+    plasmaPlot = [plasma]
+    mcellPlot = [mcell]
+    for ix in range(time):
+        dvdt = a*virus - b*plasma
+        dTdt = virus*(c + d*mcell) - e*plasma
+        dMdt = f*plasma - g*mcell
+        virus = virus + dvdt
+        plasma = plasma + dTdt
+        mcell = mcell + dMdt
+        if virus < 10: virus = 0
+        if plasma < 10: plasma = 0
+        if mcell < 10: mcell = 0 
+        # elif ix == 750:
+        #     virus = virus + 500
+        virusPlot.append(virus)
+        plasmaPlot.append(plasma)
+        mcellPlot.append(mcell)
+        if maxPlasma < plasmaPlot[ix]: maxPlasma = plasmaPlot[ix]
+    if count == 0:
+        plt.plot(np.linspace(0.06,1,time+1),np.array(plasmaPlot)/maxPlasma*4, label = 'Upper plasma')
+    else:
+        plt.plot(np.linspace(0.06,1,time+1),np.array(plasmaPlot)/maxPlasma*4, label = 'Lower plasma')
 
 
 pLinear = np.linspace(0.06,1)
 symptomLinear = pLinear*4
-
-pStep = [0,0.25,0.25,0.25,0.5,0.5,0.5,0.75,0.75,0.75,1]
-symptomStep = [0.01,0.01,0.01,1,1,1,2,2,2,3,3]
 
 plt.plot([0,1],[1,1], 'k--', alpha = 0.7)
 plt.plot([0,1],[2,2], 'k--', alpha = 0.7)
 plt.plot([0,1],[3,3], 'k--', alpha = 0.7)
 
 
-plt.plot(pStep,symptomStep, c = 'red', label = 'Step')
 
-
-plt.plot(np.linspace(0.06,1,time+1),np.array(tcellPlot)/max(tcellPlot)*4, label = 'plasma', c = 'blue')
-
-
-plt.text(0,0.1,'No symptom')
-plt.text(0.0,1.1,'Symptom expressed weakly')
-plt.text(0.0,2.1,'Moderate symptom')
-plt.text(0.0,3.5,'Sharply')
-plt.text(0.0,3.3,'expressed')
-plt.text(0.0,3.1,'symptom')
+plt.text(0,0.2,'1: No symptom')
+plt.text(0.0,1.1,'2: Symptom expressed weakly')
+plt.text(0.0,2.1,'3: Moderate symptom')
+plt.text(0.0,3.5,'4: Sharply')
+plt.text(0.0,3.3,'   expressed')
+plt.text(0.0,3.1,'   symptom')
 
 
 plt.legend()
